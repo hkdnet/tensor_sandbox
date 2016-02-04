d719a20938ba5ca18236a33e30d43161c3e2b09d
hkdnet <hkdnet@users.noreply.github.com>
2016-02-04 00:38:59 +0000

    data dir should not be tracked

--- setup_docker.sh
+++ setup_docker.sh
@@ -1,3 +1,3 @@
 eval "$(docker-machine env tensor)"
 script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
-docker run -v ${script_dir}/src:/data/tensor/ -it b.gcr.io/tensorflow/tensorflow 
+docker run -v ${script_dir}:/data/tensor/ -it b.gcr.io/tensorflow/tensorflow 
