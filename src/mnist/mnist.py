60a438d1752ddae3a0253e069bde846be2d96e52
hkdnet <hkdnet@users.noreply.github.com>
2016-02-05 01:52:40 +0900

    評価部分コピペ(後で読む)

--- src/mnist/mnist.py
+++ src/mnist/mnist.py
@@ -4,8 +4,8 @@ mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 import tensorflow as tf
 
 x = tf.placeholder(tf.float32, [None, 784])
-W = tf.Variable(tf.zeros, [784, 10])
-b = tf.Variable(tf.zeros, [10])
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
 
 # predict
 y = tf.nn.softmax(tf.matmul(x, W) + b)
@@ -17,9 +17,21 @@ cross_entropy = - tf.reduce_sum(y_ * tf.log(y))
 # training
 train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
 
+# TODO Reading
 # init
 init = tf.initialize_all_variables()
 
 # run
 sess = tf.Session()
 sess.run(init)
+
+for i in range(1000):
+  batch_xs, batch_ys = mnist.train.next_batch(100)
+  sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
